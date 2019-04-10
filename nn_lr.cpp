24cdf87a7df5ebe4163088a3aa594c30a2fc169d
qiongz <qiongzhunow@gmail.com>
2019-04-11 03:46:58 +0800

    dnn from scratch

--- nn_lr.cpp
+++ nn_lr.cpp
@@ -1,23 +1,28 @@
-#include"nn_lr.h"
+#include"dnn.h"
 #include<iostream>
 #include"utils.h"
 #define EPSILON 1e-10
 
-void nn_lr::initialize_weights() {
-    //srand(time(0));
-    W=new float[n_features*n_classes];
-    dW=new float[n_features*n_classes];
-    b=new float[n_classes];
-    db=new float[n_classes];
-    for(int i=0; i<n_features*n_classes; i++)
-        W[i]=rand()*1.0/RAND_MAX*0.01;
-    //memset(W,0,sizeof(float)*n_classes*n_features);
-    memset(b,0,sizeof(float)*n_classes);
+void dnn::initialize_weights() {
+    default_random_engein rng(weights_seed);
+    normal_distribution<float> dist_norm(0,1);
+    // n_layers-1 W,dW,b,db
+    for(int i=1;i<n_layers;i++){
+	W.push_back(new float[layer_dims[i-1]*layer_dims[i]]);
+	dW.push_back(new float[layer_dims[i-1]*layer_dims[i]]);
+	b.push_back(new float[layer_dims[i]]);
+	db.push_back(new float[layer_dims[i]]);
+	for(int j=0;j<layer_dims[i-1]*layer_dims[i];j++)
+          if(i==layers-1)
+          W[i][j]=dist_norm(rng)*sqrt(1.0/layer_dims[i-1]);
+	  else
+          W[i][j]=dis_norm(rng)*sqrt(2.0/layer_dims[i-1]);  // factor "2" for ReLU
+	memset(b[i],0,sizeof(float)*layer_dims[i]);
+    }
 }
 
-
 //Fisher and Yates' shuffle algorithm
-void nn_lr::shuffle(float *X,float *Y,int n_sample, int skip) {
+void dnn::shuffle(float *X,float *Y,int n_sample, int skip) {
     VSLStreamStatePtr rndStream;
     vslNewStream(&rndStream, VSL_BRNG_MT19937,seed);
     vslSkipAheadStream(rndStream,n_sample*(skip+1));
@@ -30,42 +35,25 @@ void nn_lr::shuffle(float *X,float *Y,int n_sample, int skip) {
         if(j!=i) {
             cblas_sswap(n_features,X+i*n_features,1,X+j*n_features,1);
             cblas_sswap(n_classes,Y+i*n_classes,1,Y+j*n_classes,1);
-	    memcpy(X_temp,X+i*n_features,sizeof(float)*n_features);
-	    memcpy(X+i*n_features,X+j*n_features,sizeof(float)*n_features);
-	    memcpy(X+j*n_features,X_temp,sizeof(float)*n_features);
-	    memcpy(Y_temp,Y+i*n_classes,sizeof(float)*n_classes);
-	    memcpy(Y+i*n_classes,Y+j*n_classes,sizeof(float)*n_classes);
-	    memcpy(Y+j*n_classes,Y_temp,sizeof(float)*n_classes);
         }
     }
     vslDeleteStream(&rndStream);
     delete Y_temp,X_temp,i_buffer;
 }
 
-void nn_lr::batch(const float* X,const float *Y,float *X_batch,float *Y_batch,int batch_size,int batch_id) {
+void dnn::batch(const float* X,const float *Y,float *X_batch,float *Y_batch,int batch_size,int batch_id) {
     cblas_scopy(n_features*batch_size,X+batch_size*batch_id*n_features,1,X_batch,1);
     cblas_scopy(n_classes*batch_size,Y+batch_size*batch_id*n_classes,1,Y_batch,1);
 }
 
-float nn_lr::cost_function(float *A,float *J,const float *Y,const int &n_sample) {
+float dnn::cost_function(float *A,float *J,const float *Y,const int &n_sample) {
     vsLn(n_sample*n_classes,A,J);
     float cost=-cblas_sdot(n_sample*n_classes,Y,1,J,1)/n_sample;
     cost+=0.5*Lambda*cblas_sdot(n_classes*n_features,W,1,W,1)/n_sample;
-    // for-loop version
-    
-    /*
-    float cost=0;
-    for(int i=0;i<n_classes*n_sample;i++)
-      cost-=Y[i]*log(A[i]+EPSILON);
-    for(int i=0;i<n_classes*n_features;i++)
-      cost+=0.5*Lambda*pow(W[i],2);
-    cost/=n_sample;
-    */
-   
     return cost;
 }
 
-float nn_lr::max(float *x,int range,int &index_max) {
+float dnn::max(float *x,int range,int &index_max) {
     float max_val=x[0];
     index_max=0;
     for(int i=1; i<range; i++)
@@ -76,161 +64,170 @@ float nn_lr::max(float *x,int range,int &index_max) {
     return max_val;
 }
 
-// softmax of A:  A[:,k]=exp(A[:,k])/\sum_j(exp(A[:,j]))
-void nn_lr::get_softmax(float *A,const int &n_sample) {
-    // calculate the softmax of A
-
+// softmax-layer  A[L-1][:,k]=exp(A[L-1][:,k])/\sum_j(exp(A[L-1][:,j]))
+void dnn::get_softmax(const int &n_sample) {
+    // calculate the softmax of A[L-1]
+    // layer_dims[n_layers-1] = n_classes
     int id_max;
-    
     for(int i=0; i<n_sample; i++) {
         // find the largest A and substract it to prevent overflow
-        float A_max=max(A+i*n_classes,n_classes,id_max);
-        for(int k=0; k<n_classes; k++) A[k+i*n_classes]-=A_max;
-        vsExp(n_classes,A+i*n_classes,A+i*n_classes);   // A:=exp(A-A_max)
-        float A_norm=1.0/cblas_sasum(n_classes,A+i*n_classes,1);
+        float A_max=max(A[n_layers-1]+i*n_classes,n_classes,id_max);
+        for(int k=0; k<n_classes; k++) A[n_layers-1][k+i*n_classes]-=A_max;
+        vsExp(n_classes,A[n_layers-1]+i*n_classes,A[n_layers-1]+i*n_classes);   // A:=exp(A-A_max)
+        float A_norm=1.0/cblas_sasum(n_classes,A[n_layers-1]+i*n_classes,1);
         for(int k=0; k<n_classes; k++)
-            A[i*n_classes+k]*=A_norm;   // A:=exp(A-A_max)/\sum_i(exp(A_i-A_max))
+            A[n_layers-1][i*n_classes+k]*=A_norm;   // A:=exp(A-A_max)/\sum_i(exp(A_i-A_max))
     }
-    
-
-    // for-loop version for double-check
-    /*
-    for(int i=0;i<n_sample;i++){
-    float A_max=max(A+i*n_classes,n_classes,id_max);
-    float A_norm=0;
-    for(int k=0;k<n_classes;k++) {
-    	A[k+i*n_classes]-=A_max;
-    	A[k+i*n_classes]=exp(A[k+i*n_classes]);
-    	A_norm+=A[k+i*n_classes];
-    }
-    A_norm=1.0/A_norm;
-        for(int k=0;k<n_classes;k++)
-       A[i*n_classes+k]*=A_norm;   // A:=exp(A-A_max)/\sum_i(exp(A_i-A_max))
-    }
-    */
-   
 }
 
-void nn_lr::gradient_approx(float *A,float *J,const float *X,const float *Y,const int &n_sample) {
-    float epsilon=1e-5;
+/*
+void dnn::gradient_approx(float *A,float *J,const float *X,const float *Y,const int &n_sample) {
+    float epsilon=1e-4;
     float delta_cost;
+    float W_orig,b_orig;
     for(int i=0; i<n_classes; i++)
         for(int j=0; j<n_features; j++) {
+	    W_orig=W[j+i*n_features];
+
             W[j+i*n_features]+=epsilon;
             forward_propagate(A,X,n_sample);
             get_softmax(A,n_sample);
             delta_cost=cost_function(A,J,Y,n_sample);
+
             W[j+i*n_features]-=2*epsilon;
             forward_propagate(A,X,n_sample);
             get_softmax(A,n_sample);
             delta_cost-=cost_function(A,J,Y,n_sample);
+
             dW[j+i*n_features]=delta_cost*0.5/epsilon;
-            W[j+i*n_features]+=epsilon;
+            W[j+i*n_features]=W_orig;
         }
     for(int j=0; j<n_classes; j++) {
+	b_orig=b[j];
+
         b[j]+=epsilon;
         forward_propagate(A,X,n_sample);
         get_softmax(A,n_sample);
         delta_cost=cost_function(A,J,Y,n_sample);
+
         b[j]-=2*epsilon;
         forward_propagate(A,X,n_sample);
         get_softmax(A,n_sample);
         delta_cost-=cost_function(A,J,Y,n_sample);
 
         db[j]=delta_cost*0.5/epsilon;
-        b[j]+=epsilon;
+        b[j]=b_orig;
     }
 }
+*/
 
-// Vectorized version of forward_propagate  for one-layer
-// A=X*W.T+b
-void nn_lr::forward_propagate(float *A,const float *X,const int &n_sample) {
+void dnn::sigmoid_activate(const int &l,const int &n_sample){
+    for(int i=0;i<layer_dims[l]*n_sample;i++)
+       A[l][i]=-A[l][i];
+    vsExp(layer_dims[l]*n_sample,A[l],A[l]);
+    for(int i=0;i<layer_dims[l]*n_sample;i++)
+       A[l][i]+=1;
+    vsDiv(layer_dims[l]*n_sample,A[l],A[l]);
+}
+
+// Vectorized version of forward_activated_propagate for each layer
+// A[l]=activate(Z[l-1]*W.T+b)
+void dnn::forward_activated_propagate(const int &l, const int &n_sample) {
     float alpha_=1;
     float beta_=0;
-    
-    // Forward propagation (from X to A1)
-    // A(n_sample,n_classes) := X(n_sample,n_features) * W(n_classes, n_features).T,
-    // A[j+i*n_classes]= sum_k (X[k+i*n_features]*W[k+j*n_features])
-    // A(i,j): a[i+j*lda] for ColMajor, a[j+i*lda] for RowMajor, lda is the leading dimension
-    // op(X)(n_sample,n_features), op(W)( n_features,n_classes)
-    cblas_sgemm(CblasRowMajor,CblasNoTrans,CblasTrans,n_sample,n_classes,n_features,
-                alpha_, X, n_features, W, n_features, beta_, A, n_classes);
+    // linear forward propagation (from A[l-1] to Z[l])
+    // Z[l](n_sample,n_l) := A[l-1](n_sample,n_{l-1}) * W(n_l, n_{l-1}).T,
+    // Z(i,j): a[i+j*lda] for ColMajor, a[j+i*lda] for RowMajor, lda is the leading dimension
+    cblas_sgemm(CblasRowMajor,CblasNoTrans,CblasTrans,n_sample,layer_dims[l],layer_dims[l-1],
+                alpha_, A[l-1], layer_dims[l-1], W[l], layer_dims[l-1], beta_, A[l], layer_dims[l]);
 
-    // A(n_sample,n_classes) := A(n_sample,n_classes)+b(:,n_classes)
+    // Z[l]:= Z[l]+b[l]
     for(int i=0; i<n_sample; i++)
-        vsAdd(n_classes,A+i*n_classes,b,A+i*n_classes);
-   
+        vsAdd(layer_dims[l],A[l]+i*layer_dims[l],b[l],A[l]+i*layer_dims[l]);
 
-    // for-loop version for double-check
-    /*   
-    for(int i=0;i<n_sample;i++)
-      for(int j=0;j<n_classes;j++){
-        A[i*n_classes+j]=0;
-        for(int k=0;k<n_features;k++)
-          A[i*n_classes+j]+=X[k+i*n_features]*W[k+j*n_features];
-        A[i*n_classes+j]+=b[j];
-      }
-    */ 
+    // final output: A[l]= f(Z[l])
+    if(activation_types[l-1]=="sigmoid")
+	sigmoid_activate(l,n_sample);
 }
 
-// Vectorized version of backward_propagate
-void nn_lr::backward_propagate(float *A, const float *X, const float *Y,const int &n_sample) {
-    float alpha_=1.0/n_sample;
+// Vectorized version of backward_propagate, 
+// (all dA[l-1],dW[l] is shifted by 1: dA[l-1]-> dA[l-2], dW[l]-> dW[l-1] 
+// dJ/dA[l-1]=dJ/dA[l]*dA[l]/dA[l-1]= dJ/dA[l]*f'(Z[l-1])*W[l].T
+// dJ/dW[l]=dJ/dA[l]*dA[l]/dW[l-1]= dJ/dA[l]*f'(Z[l-1])*A[l-1].T
+// dJ/db[l-1]=dJ/dA[l]*dA[l]/db[l-1]= dJ/dA[l]*f'(Z[l-1])
+void dnn::backward_propagate(const int & l,const int &n_sample) {
+    // create tempory space for f'() factor
+    float* dF=new float[layer_dims[l]*n_sample];
+    float alpha_=1;
     float beta_=0;
-   
-    // Backward propagation (get gradients)
-    vsSub(n_sample*n_classes,A,Y,A);     // dA(i,k):=A(i,k)-1{Y(i,k)==1}
-    // dW(n_classes,n_features):= dA(n_sample,n_classes).T * X(n_sample,n_features)
+    if(activation_types[l-1]=="sigmoid"){
+    	// for sigmoid activation function, f'(z)=f(z)(1-f(z))    
+	vsMul(layer_dims[l]*n_sample,A[l],A[l],dF);  // dF= A.*A
+	vsSub(layer_dims[l]*n_sample,dF,A[l],dF);     // dF=dF-A
+    }
+    // calculate dJ/dA[l]*f'(Z[l-1]), the previous dA[l-1] (or 'dA_{l}') is overwritten and pass down
+    vsMul(layer_dims[l]*n_sample,dA[l-1],dF,dA[l-1]);   // dA[l-1]=dA[l].*dF
+    // calculate db[l]:=sum(dA[l](n_sample,n_l),axis=0)
+    for(int i=0;i<layer_dims[l];i++){
+        db[l][i]=0;
+        for(int j=0; j<n_sample; j++)
+            db[l][i]+=A[j*layer_dims[l]+i];
+        db[l][i]/=n_sample;
+    } 
+
+    // dJ/dW[l] = dJ/dA[l]*f'(Z[l-1])*A[l-1].T
+    // dW[l](n_l,n_{l-1}):= dF(n_sample,n_l).T * A[l-1](n_sample,n_{l-1})
     // op(dA)(n_class,n_sample), op(X)(n_sample,n_features)
-    cblas_sgemm (CblasRowMajor,CblasTrans,CblasNoTrans,n_classes,n_features,n_sample,
-                 alpha_, A, n_classes, X, n_features, beta_, dW, n_features);
+    cblas_sgemm (CblasRowMajor,CblasTrans,CblasNoTrans,layer_dims[l],layer_dim[l-1],n_sample,
+                 alpha_, dA[l], layer_dims[l], A[l-1], layer_dims[l-1], beta_, dW[l], layer_dims[l-1]);
+
     // dW:=dW+Lambda*W
     if(Lambda>1e-8)
-        cblas_saxpy(n_classes*n_features,Lambda/n_sample,W,1,dW,1);
+        cblas_saxpy(layer_dims[l]*layer_dims[l-1],Lambda/n_sample,W[l],1,dW[l],1);
 
-    // db(n_classes):= sum(dA(n_sample,n_classes),axis=0)
-    for(int i=0; i<n_classes; i++) {
-        db[i]=0;
-        for(int j=0; j<n_sample; j++)
-            db[i]+=A[j*n_classes+i];
-        db[i]/=n_sample;
-    }
-   
+    // dJ/dA[l-1] = dJ/dA[l]*f'(Z[l-1])*W[l].T
+    cblas_sgemm (CblasRowMajor,CblasTrans,CblasNoTrans,n_sample,layer_dim[l-1],layer_dim[l],
+                 alpha_, dA[l], layer_dims[l], W[l], layer_dims[l-1], beta_, dA[l-1], layer_dims[l-1]);
 
-
-    // for-loop version for double-check
-    /*
-     for(int i=0;i<n_sample*n_classes;i++)
-    A[i]=A[i]-Y[i];
-     for(int i=0;i<n_classes;i++)
-    for(int j=0;j<n_features;j++){
-      dW[i*n_features+j]=Lambda*W[i*n_features+j];
-      for(int k=0;k<n_sample;k++)
-      dW[i*n_features+j]+=A[k*n_classes+i]*X[k*n_features+j];
-      dW[i*n_features+j]/=n_sample;
-    }
-  */ 
 }
 
-void nn_lr::fit(const vector<float> &_X,const vector<int> &_Y,const int &n_sample, const int num_epochs=2000,const float learning_rate=0.01,const float _lambda=0,int batch_size=128,bool print_cost=false) {
+void dnn::multi_layers_forward(const int &n_sample){
+    for(int l=1;l<n_layers;l++)
+      forward_propagate(l,n_sample);
+    get_softmax(n_sample);
+}
+
+void dnn::multi_layers_backward(const float *Y,const int &n_sample){
+     // softmax layer: dJ/dA[L-2]=(A-Y)/n_sample (dA has dimsion L-1
+     float alpha_=1.0/n_sample;
+     vsSub(n_sample*layer_dims[n_layers-1],A[n_layers-1],Y,dA[n_layers-2]);
+     for(int i=0;i<n_sample*layer_dims[n_layers-1];i++)  dA[n_layers-2][i]*=alpha;
+
+     // get gradients
+     for(int l=n_layers-1;l>=1;l--)
+	backward_propagate(l,n_sample);
+}
+
+
+void dnn::fit(const vector<float> &_X,const vector<int> &_Y,const int &n_sample, const int num_epochs=2000,const float learning_rate=0.01,const float _lambda=0,int batch_size=128,bool print_cost=false) {
     Lambda=_lambda;
     // tempory space for storing the shuffled training data sets
     float *X=new float[_X.size()];
     float *Y=new float[_Y.size()];
+
     // deep copy X,Y
     for(int i=0; i<_X.size(); i++) X[i]=_X[i];
     for(int i=0; i<_Y.size(); i++) Y[i]=_Y[i];
 
-    // batch training data sets
-    float *X_batch=new float[batch_size*n_features];
-    float *Y_batch=new float[batch_size*n_classes];
-    // tempory space used for vectorized calculation of b,cost
-    float *J=new float[batch_size*n_classes];
-    // activation layer
-    float *A=new float[batch_size*n_classes];
+    // initialize layer caches and layer gradients
+    for(int l=0;l<n_layers;l++)
+       A.push_back(new float[layer_dims[l]*batch_size]);
+    // the first-layer of dA is not calculated
+    for(int l=1;l<n_layers;l++)   
+       dA.push_back(new float[layer_dims[l]*batch_size]);
 
-    float *dW_approx=new float [n_classes*n_features];
-    float *db_approx=new float [n_classes];
+    // batch training data sets
+    float *Y_batch=new float[batch_size*n_classes];
     float cost,cost_batch;
     for(int i=0; i<num_epochs; i++) {
         // shuffle training data sets for one epoch
@@ -238,31 +235,13 @@ void nn_lr::fit(const vector<float> &_X,const vector<int> &_Y,const int &n_sampl
         cost=0;
         // batch training until all the data sets are used
         for(int s=0; s<n_sample/batch_size; s++) {
-            batch(X,Y,X_batch,Y_batch,batch_size,s);
+            // feed A[0] with X_batch
+            batch(X,Y,A[0],Y_batch,batch_size,s);
             // forward_propagate and calculate the activation layer
-            forward_propagate(A,X_batch,batch_size);
-            get_softmax(A,batch_size);
-            cost_batch=cost_function(A,J,Y_batch,batch_size);
-            //gradient_approx(A,J,X_batch,Y_batch,batch_size);
-            //memcpy(dW_approx,dW,sizeof(float)*n_classes*n_features);
-            //memcpy(db_approx,db,sizeof(float)*n_classes);
-            backward_propagate(A,X_batch,Y_batch,batch_size);
-	   /* 
-            cout<<"dW difference"<<endl;
-            for(int i=0; i<n_classes; i++) {
-                for(int j=0; j<n_features; j++) {
-                    float diff=dW_approx[i*n_features+j]-dW[j+i*n_features];
-                    if(abs(diff/(dW_approx[i*n_features+j]+1e-8))>1e-5)
-                        cout<<diff/(dW_approx[j+i*n_features]+1e-8)<<"  ";
-                }
-                cout<<endl;
-            }
-	    */
-	    
-	   
-            // update the weights and bias
-            cblas_saxpy(n_classes*n_features,-learning_rate,dW,1,W,1);   // W:=W-learning_rate*dW
-            cblas_saxpy(n_classes,-learning_rate,db,1,b,1);              // b:=b-learning_rate*db
+	    multi_layers_forward(batch_size);
+            cost_batch=cost_function(Y_batch,batch_size);
+	    multi_layers_backward(Y_batch,batch_size);
+	    weights_update(learning_rate);
             cost+=cost_batch;
         }
 
@@ -275,7 +254,7 @@ void nn_lr::fit(const vector<float> &_X,const vector<int> &_Y,const int &n_sampl
     delete A,J,Y_batch,X_batch,Y,X;
 }
 
-void  nn_lr::predict(const vector<float>& _X,vector<int> &Y_prediction,const int &n_sample) {
+void  dnn::predict(const vector<float>& _X,vector<int> &Y_prediction,const int &n_sample) {
     float *X=new float[_X.size()];
     float *A=new float[n_sample*n_classes];
     for(int i=0; i<_X.size(); i++) X[i]=_X[i];
@@ -295,7 +274,7 @@ void  nn_lr::predict(const vector<float>& _X,vector<int> &Y_prediction,const int
 }
 
 
-float nn_lr::predict_accuracy(const vector<float>& _X,const vector<int> & Y,vector<int> &Y_prediction,const int &n_sample) {
+float dnn::predict_accuracy(const vector<float>& _X,const vector<int> & Y,vector<int> &Y_prediction,const int &n_sample) {
     float *X=new float[_X.size()];
     float *A=new float[n_sample*n_classes];
     for(int i=0; i<_X.size(); i++) X[i]=_X[i];
--- nn_lr.h
+++ nn_lr.h
@@ -1,31 +1,48 @@
 #include<vector>
 #include<cstdlib>
 #include<cstring>
+#include<string>
 #include<cmath>
 #include<cstdio>
 #include<ctime>
+#include<random>
 #include"mkl.h"
 #define METHOD_INT VSL_RNG_METHOD_UNIFORMBITS_STD
 
 using namespace std;
-class nn_lr
+class dnn
 {
 public:
-    nn_lr(int n_f,int n_c):n_features(n_f),n_classes(n_c) {
+    // default initializer without hidden-layers
+    dnn(int n_f,int n_c):n_features(n_f),n_classes(n_c) {
+	n_layers=2;
         initialize_weights();
+	layer_dims.push_back(n_features);
+	layer_dims.push_back(n_classes);
 	seed=time(0);
     }
-    ~nn_lr() {};
+    dnn(int n_f,int n_c,int n_h,const vector<int>& dims,const vector<string> act_types):n_features(n_f),n_classes(n_c),n_layers(n_h+2){
+	layer_dims.push_back(n_features);
+	for(int i=0;i<n_h;i++) layer_dims.push_back(dims[i]);
+	layer_dims.push_back(n_classes);
+        initialize_weights();
+	activations_types.assign(act_types);
+	seed=time(0);
+    }
+    ~dnn() {};
     void fit(const vector<float>&,const vector<int>&,const int &,const int,const
              float,float,int,bool);
     void predict(const vector<float>&,vector<int> &,const int&);
     float predict_accuracy(const vector<float>&,const vector<int> &,vector<int> &,const int&);
 
 private:
-    int n_features,n_classes;
+    int n_features,n_classes,n_layers;
     float Lambda;
-    float *W,*dW,*b,*db;
-    unsigned seed;
+    vector<float*> dW,db;
+    vector<float*> A,dA;
+    vector<int> layer_dims;
+    vector<string> activation_types;
+    unsigned weights_seed;
 
     void shuffle(float *X,float *Y,int n_sample, int skip);
     void batch(const float *X,const float *Y,float *X_batch,float *Y_batch,int batch_size, int batch_id);
